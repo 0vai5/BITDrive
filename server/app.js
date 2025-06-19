77c78682fb0f84a1db136cd0b6ee0a18b4c76d83
0vai5 <rovais53@gmail.com>
2025-06-19 15:24:34 +0500

    Feat: Simplify CORS origin configuration to allow only production URL

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bitdrive.vercel.app"],
+    origin: "https://bitdrive.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
